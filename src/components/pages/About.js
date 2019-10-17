445d26d30416dd599f969e9517370289e267985f
Chinedu Emesue <de.magnificent1@gmail.com>
2019-10-17 16:09:45 +0100

    latest update

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,13 +3,16 @@ import React from 'react';
 function About() {
     return (
         <React.Fragment>
+            <br></br>
+            <br></br>
            <h1>About</h1> 
-           <p>This is the TodoList app v1.0.0. It is my first React Js App</p>
-           <p>Follow on social media:</p>
-           <p>Twitter: <a href="../https//www.twitter.com/its_nedum" target="_blank">@its_nedum</a></p>
-           <p>Instagram: <a href="../https//www.instagram.com/its_nedum" target="_blank">@its_nedum</a></p>
-           <p>Facebook: <a href="../https//www.facebook.com/itsnedum" target="_blank">@itsnedum</a></p>
-           <p>Github: <a href="../https//github.com/its-nedum" target="_blank">@its-nedum</a></p>
+           <p>This is a my React Js Todo list App v1.0.0</p>
+           <br></br>
+           <p>Follow on:</p>
+           <p>Github: @its-nedum</p>
+           <p>Twitter: @its_nedum</p>
+           <p>Instagram: @its_nedum</p>
+           <p>Facebook: @itsnedum</p>
         </React.Fragment>
     )
 }
