18d21ed9fd75b8bffb5c6126be174791d563832c
Chinedu Emesue <de.magnificent1@gmail.com>
2019-10-17 16:11:40 +0100

    latest update

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,7 +8,7 @@ function About() {
            <h1>About</h1> 
            <p>This is a my React Js Todo list App v1.0.0</p>
            <br></br>
-           <p>Follow on:</p>
+           <p>Follow me on:</p>
            <p>Github: @its-nedum</p>
            <p>Twitter: @its_nedum</p>
            <p>Instagram: @its_nedum</p>
